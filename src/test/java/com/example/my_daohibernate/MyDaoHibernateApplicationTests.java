367fa10a668d9d1a37dfc9fe1c396e71656dbf49
Boris Rozov <outsource@list.ru>
2023-08-07 22:29:31 +0300

    Initial commit

---
+++ src/test/java/com/example/my_daohibernate/MyDaoHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_daohibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDaoHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
